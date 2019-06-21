1bbb8ea3c564402e3fa7a988f6b60bfcc61be497
Kamelia <kamelia.903@gmail.com>
2019-06-22 02:56:20 +0530

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'BankApplication';
+  apiData;
+  apiDataObj;
+  headers;
+  searchModel;
+  itemsperpage;
+  idx;
+  currentCity;
+  pageNo;
+  
+  constructor(private http: HttpClient){
+    this.initData("MUMBAI") ; //default is MUMBAI
+  }
+  initData(city){
+    city = city.toUpperCase();
+    this.currentCity = city;
+    const path = "https://vast-shore-74260.herokuapp.com/banks?city="+city; 
+    if(!localStorage[city]){
+      this.http.get(path)
+      .subscribe(response =>{
+        if(response){
+          this.apiData = response;
+          this.setupData();
+          localStorage[city] = JSON.stringify(this.apiData);
+        }  
+      });
+    }
+    else{
+      this.apiData = JSON.parse(localStorage[city]);
+      this.setupData();
+    } 
+  }
+
+  findIndexOfFav(isFav){
+    return isFav === "isFav";
+  }
+
+   setupData(){
+    for(let i =0; i< this.apiData.length; i++){
+      if(!this.apiData[i].isFav){
+        this.apiData[i].isFav = false;
+      }
+    }
+    this.headers = Object.keys(this.apiData[0]);
+    this.idx = this.headers.findIndex(this.findIndexOfFav);
+     this.itemsperpage = 50;
+  }
+  objectValues(obj){
+    return  Object.values(obj);
+  }
+  changePageLength(event){
+    this.itemsperpage = (!!Number(event.target.value))?Number(event.target.value): 50;
+  }
+  updateFav(data){
+    localStorage[this.currentCity] = JSON.stringify(this.apiData);
+  }
+  onChangeCity(event){
+    this.currentCity = (!!event.target.value)? event.target.value.toUpperCase(): "";
+    this.initData(this.currentCity);
+  }
+  updatepageNo(event){
+    this.pageNo = event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPaginationModule,
+    FormsModule,
+    Ng2SearchPipeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
